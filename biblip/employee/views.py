de74e218daad2ae13331b9b8d502a3f9bca56660
Gabriella-Aquino <gabyyaquino8@gmail.com>
2024-12-14 13:56:10 -0300

    feature: estender base e adicionar class de estilo base para pagina employee

--- biblip/employee/views.py
+++ biblip/employee/views.py
@@ -8,7 +8,7 @@ def borrow_management(request):
     json_path_temp = os.path.join(settings.BASE_DIR, 'employee', 'appointment.json')
     with open (json_path_temp, 'r') as file:
         appointments = json.load(file)
-    context = {'appointments': appointments}
+    context = {'appointments': appointments, 'employer': True}
     return render(request, 'index.html', context)
 
 def create_book(request):
