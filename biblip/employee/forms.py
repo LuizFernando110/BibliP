cf0ced7a3a4c66b1db257b49363472bcc8eb840c
LuizFernando110 <luizloona@gmail.com>
2025-01-13 15:17:02 -0300

    Fix: Adição do input de gênero ao formulário e reajuste nos tamanhos dos elementos.

--- biblip/employee/forms.py
+++ biblip/employee/forms.py
@@ -5,6 +5,7 @@ class bookRegisterForm(forms.Form):
     book_title = forms.CharField(max_length = 100)
     writer = forms.CharField(max_length = 100)
     publisher = forms.CharField(max_length = 100)
+    genrer = forms.CharField(max_length = 100)
     edition = forms.IntegerField()
     number_of_pages = forms.IntegerField()
     pnld_code = forms.CharField(max_length = 100)
