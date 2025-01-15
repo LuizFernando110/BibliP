6769a0102119c1105264410421252fa3c90eb11e
Gabriella-Aquino <gabyyaquino8@gmail.com>
2025-01-15 13:46:48 -0300

    fix: concerta url de detalhes do emprestimo na parte de professores

--- biblip/core/urls.py
+++ biblip/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index,search,details,borrow,borrow_history, login_teacher, user_register
+from .views import index,search,details,borrow,borrow_history, login_teacher, borrow_details, user_register
 
 urlpatterns = [
     path('',index,name='index'),
@@ -7,6 +7,7 @@ urlpatterns = [
     path('collection/details/<int:book_pk>',details,name='details'),
     path('collection/borrow/<int:book_pk>',borrow,name='borrow'),
     path('collection/history/borrow_history',borrow_history,name='borrow_history'),
+    path('collection/history/borrow_history/borrow_details/<int:borrow_pk>',borrow_details,name='borrow_history_details'),
     path('login',login_teacher,name='login_teacher'), 
     path('register',user_register,name='user_register'),
 ]
