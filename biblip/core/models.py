2073db5340eb68d88662380865250170de1077ff
Gabriella-Aquino <gabyyaquino8@gmail.com>
2025-01-16 19:13:53 -0300

    feature: adiciona models de Sudent e SchoolClass em core

--- biblip/core/models.py
+++ biblip/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from employee.models import Profile
+
+
+class Student(models.Model):
+    student_name=models.CharField(max_length=244)
+    student_registration=models.CharField(max_length=14)
+
+
+    def _str_(self):
+        return self.student_name
+
+class SchoolClass(models.Model):
+    school_class_name=models.CharField(max_length=244)
+    school_class_teacher=models.ForeignKey(Profile)
+    school_class_students=models.ManyToManyField(Student)
+
+    def _str_(self):
+        return self.school_class_name
