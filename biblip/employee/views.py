9f726508eeaf84766b9a9a53f32d8b05a9203bef
Gabriella-Aquino <gabyyaquino8@gmail.com>
2025-02-18 19:19:03 -0300

    refactor: muda condição dev linkagem de historico de employer no contexto para profile_type no request

--- biblip/employee/views.py
+++ biblip/employee/views.py
@@ -81,18 +81,13 @@ class employer_borrow_list(ListView):
             if self.request.GET.get('appointments_type')=='week':
                 appointments=Borrow.objects.filter(borrow_delivery_date__range=(first_day_of_the_week,last_day_of_the_week))
 
-        return {'borrow_history':appointments,'employer':True, 'filter_title':'Histórico de aluguéis'}
+        return {'borrow_history':appointments, 'filter_title':'Histórico de aluguéis'}
 
 
 class employer_borrow_details(DetailView):
     model=Borrow
     pk_url_kwarg='borrow_pk'  
     template_name='employer_borrow_details.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['employer'] = True
-        return context
         
 
 
