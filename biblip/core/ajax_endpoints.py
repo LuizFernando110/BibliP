742d67b5bdf6fb4b50d69325df2d1683ac5e54ad
Linkzandro <lizandrolgui123@gmail.com>
2025-02-21 21:31:22 -0300

    refactor: borrow_history

--- biblip/core/ajax_endpoints.py
+++ biblip/core/ajax_endpoints.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
-from employee.models import Borrow
+from employee.models import Borrow, BorrowStudent
 from employee.borrow_evaluations import borrow_close_evaluation
-from datetime import date,timedelta
+from datetime import date
 
 
 def cancel_borrow(request,borrow_pk):
@@ -14,13 +14,77 @@ def cancel_borrow(request,borrow_pk):
 
         borrow.save()
         
-        
+        book=borrow.borrow_book
+        book.book_status=1
+        book.save()
+
         return JsonResponse({
             
             'success':True,
             'borrow':{
                 'data_entrega':borrow.borrow_delivery_date.strftime('%d/%m/%Y'),
             }
+        })
+    else:
+        return JsonResponse({
+            'success':False,
+        })
+    
+def accept_borrow(request,borrow_pk):
+    borrow=Borrow.objects.get(id=borrow_pk)
+    if borrow.borrow_status ==1:
+        borrow.borrow_status=2
+        borrow.save()
+
+        
+        return JsonResponse({
+            
+            'success':True,
+        })
+    else:
+        return JsonResponse({
+            'success':False,
+        })
+
+def student_borrow_receipt(request,borrow_student_pk):
+    borrow_student=BorrowStudent.objects.get(id=borrow_student_pk)
+
+
+    if borrow_student.borrow_student_status==1:
+        borrow_student.borrow_student_status=2
+        borrow_student.save()
+
+        return JsonResponse({
+            
+            'success':True,
+        })
+    else:
+        return JsonResponse({
+            'success':False,
+        })
+    
+def student_borrow_deliver(request,borrow_student_pk):
+    borrow_student=BorrowStudent.objects.get(id=borrow_student_pk)
+    book=borrow_student.borrow_holder.borrow_book
+    if borrow_student.borrow_student_status==2:
+        borrow_student.borrow_student_status=3
+        borrow_student.save()
+
+        book.number_available+=1
+        book.save()
+        return JsonResponse({
+                
+                'success':True,
+            })
+    
+    elif borrow_student.borrow_student_status==4:
+        borrow_student.borrow_student_status=5
+        borrow_student.save()
+        book.number_available+=1
+        book.save()
+        return JsonResponse({
+            
+            'success':True,
         })
     else:
         return JsonResponse({
