43ae4ba99f58429793b52f0bc0f0ec03aae07af0
Gabriella-Aquino <gabyyaquino8@gmail.com>
2025-02-23 09:41:32 -0300

    feature: cria is_teacher decorator

---
+++ biblip/core/decorators.py
@@ -0,0 +1,14 @@
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse
+
+def is_teacher(function, redirect_name='login'):
+  def wrapper(request,  *args, **kw):
+    if request.user.is_anonymous:
+      return HttpResponseRedirect(reverse(redirect_name))
+    
+    profile = request.user.profile
+
+    if not (profile.profile_type == 1):
+      return HttpResponseRedirect(reverse(redirect_name))
+    else:
+      return function(request, *args, **kw)
